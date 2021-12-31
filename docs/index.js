bdf100177e2d8f6c6fb72a104f4bee1214595d04
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2021-12-31 15:32:42 +0900

    minifiy js

--- docs/index.js
+++ docs/index.js
@@ -6,21 +6,13 @@ function remaindTime() {
     if (nowSec < endSec) {
         document.querySelector("#title p").innerHTML = "새해까지 남은시간"
         document.querySelector("#timer").style.display = "flex"
-
         let left = new Date(endSec-nowSec)
         let sec  = left.getSeconds()
         let min  = left.getMinutes()
         let hour = left.getUTCHours()
-
-        if (hour < 10) {
-            hour = "0" + hour;
-        }
-        if (min < 10) {
-            min = "0" + min;
-        }
-        if (sec < 10) {
-            sec = "0" + sec;
-        }
+        if (hour < 10) hour = "0" + hour
+        if (min < 10) min = "0" + min
+        if (sec < 10) sec = "0" + sec
         document.querySelector(".hours").innerHTML = hour
         document.querySelector(".minutes").innerHTML = min
         document.querySelector(".seconds").innerHTML = sec
